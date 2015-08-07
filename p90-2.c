c89ce558f74ee3702a8af29d740d08179f80f7a2
evan <evan886@gmail.com>
2015-08-07 17:30:56 +0800

    2015年 08月 07日 星期五 17:30:56 CST

---
+++ p90-2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i ;
+	int vc[5] ;   /* 包含5个元素的数组*/
+
+	for (i = 0; i<5; i++)
+		vc[i] = i +1;
+
+	for(i = 0; i<5; i++)
+		printf("vc[%d]= %d \n", i,vc[i]);
+
+	return (0);
+}
+
+/*vc[0]= 1
+vc[1]= 2
+vc[2]= 3
+vc[3]= 4
+vc[4]= 5 */
