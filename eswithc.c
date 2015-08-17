c312c9da88bb79543972cd9f530e8a76a56e98e8
evan <evan886@gmail.com>
2015-08-17 10:26:23 +0800

    2015年 08月 17日 星期一 10:26:23 CST

---
+++ eswithc.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void)
+{
+	 int a=2;
+	 switch (a)
+	 {
+	 case 1 : printf("%d \n",a);
+	 case 2 : printf("%d \n",a);
+	 case 3 : printf("%d \n",a);
+	 }
+}
+
+
+/*
+2
+2
+*/
