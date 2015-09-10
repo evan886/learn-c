d37390f1100ba198266478ab2d72b805a4caee56
evan <evan886@gmail.com>
2015-09-10 15:23:32 +0800

    2015年 09月 10日 星期四 15:23:32 CST

--- limits.h
+++ limits.h
@@ -1,7 +1,7 @@
-/* Copyright (C) 1991-2014 Free Software Foundation, Inc.
+/* Copyright (C) 1991, 1992, 1996, 1997, 1998, 1999, 2000, 2005
+   Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
-evan@evankali:~$ cat /usr/include/limits.h 
    The GNU C Library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
    License as published by the Free Software Foundation; either
@@ -13,15 +13,16 @@ evan@evankali:~$ cat /usr/include/limits.h
    Lesser General Public License for more details.
 
    You should have received a copy of the GNU Lesser General Public
-   License along with the GNU C Library; if not, see
-   <http://www.gnu.org/licenses/>.  */
+   License along with the GNU C Library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+   02111-1307 USA.  */
 
 /*
- *  ISO C99 Standard: 7.10/5.2.4.2.1 Sizes of integer types <limits.h>
+ *	ISO C99 Standard: 7.10/5.2.4.2.1 Sizes of integer types	<limits.h>
  */
 
 #ifndef _LIBC_LIMITS_H_
-#define _LIBC_LIMITS_H_ 1
+#define _LIBC_LIMITS_H_	1
 
 #include <features.h>
 
@@ -29,7 +30,7 @@ evan@evankali:~$ cat /usr/include/limits.h
 /* Maximum length of any multibyte character in any locale.
    We define this value here since the gcc header does not define
    the correct value.  */
-#define MB_LEN_MAX  16
+#define MB_LEN_MAX	16
 
 
 /* If we are not using GNU CC we have to define all the symbols ourself.
@@ -40,7 +41,7 @@ evan@evankali:~$ cat /usr/include/limits.h
    #include's protect themselves, and in GCC 2 we may #include_next through
    multiple copies of this file before we get to GCC's.  */
 # ifndef _LIMITS_H
-#  define _LIMITS_H 1
+#  define _LIMITS_H	1
 
 #include <bits/wordsize.h>
 
@@ -50,69 +51,69 @@ evan@evankali:~$ cat /usr/include/limits.h
 /* These assume 8-bit `char's, 16-bit `short int's,
    and 32-bit `int's and `long int's.  */
 
-/* Number of bits in a `char'.  */
-#  define CHAR_BIT  8
+/* Number of bits in a `char'.	*/
+#  define CHAR_BIT	8
 
 /* Minimum and maximum values a `signed char' can hold.  */
-#  define SCHAR_MIN (-128)
-#  define SCHAR_MAX 127
+#  define SCHAR_MIN	(-128)
+#  define SCHAR_MAX	127
 
 /* Maximum value an `unsigned char' can hold.  (Minimum is 0.)  */
-#  define UCHAR_MAX 255
+#  define UCHAR_MAX	255
 
 /* Minimum and maximum values a `char' can hold.  */
 #  ifdef __CHAR_UNSIGNED__
-#   define CHAR_MIN 0
-#   define CHAR_MAX UCHAR_MAX
+#   define CHAR_MIN	0
+#   define CHAR_MAX	UCHAR_MAX
 #  else
-#   define CHAR_MIN SCHAR_MIN
-#   define CHAR_MAX SCHAR_MAX
+#   define CHAR_MIN	SCHAR_MIN
+#   define CHAR_MAX	SCHAR_MAX
 #  endif
 
 /* Minimum and maximum values a `signed short int' can hold.  */
-#  define SHRT_MIN  (-32768)
-#  define SHRT_MAX  32767
+#  define SHRT_MIN	(-32768)
+#  define SHRT_MAX	32767
 
 /* Maximum value an `unsigned short int' can hold.  (Minimum is 0.)  */
-#  define USHRT_MAX 65535
+#  define USHRT_MAX	65535
 
 /* Minimum and maximum values a `signed int' can hold.  */
-#  define INT_MIN   (-INT_MAX - 1)
-#  define INT_MAX   2147483647
+#  define INT_MIN	(-INT_MAX - 1)
+#  define INT_MAX	2147483647
 
 /* Maximum value an `unsigned int' can hold.  (Minimum is 0.)  */
-#  define UINT_MAX  4294967295U
+#  define UINT_MAX	4294967295U
 
 /* Minimum and maximum values a `signed long int' can hold.  */
 #  if __WORDSIZE == 64
-#   define LONG_MAX 9223372036854775807L
+#   define LONG_MAX	9223372036854775807L
 #  else
-#   define LONG_MAX 2147483647L
+#   define LONG_MAX	2147483647L
 #  endif
-#  define LONG_MIN  (-LONG_MAX - 1L)
+#  define LONG_MIN	(-LONG_MAX - 1L)
 
 /* Maximum value an `unsigned long int' can hold.  (Minimum is 0.)  */
 #  if __WORDSIZE == 64
-#   define ULONG_MAX    18446744073709551615UL
+#   define ULONG_MAX	18446744073709551615UL
 #  else
-#   define ULONG_MAX    4294967295UL
+#   define ULONG_MAX	4294967295UL
 #  endif
 
 #  ifdef __USE_ISOC99
 
 /* Minimum and maximum values a `signed long long int' can hold.  */
-#   define LLONG_MAX    9223372036854775807LL
-#   define LLONG_MIN    (-LLONG_MAX - 1LL)
+#   define LLONG_MAX	9223372036854775807LL
+#   define LLONG_MIN	(-LLONG_MAX - 1LL)
 
 /* Maximum value an `unsigned long long int' can hold.  (Minimum is 0.)  */
-#   define ULLONG_MAX   18446744073709551615ULL
+#   define ULLONG_MAX	18446744073709551615ULL
 
 #  endif /* ISO C99 */
 
-# endif /* limits.h  */
-#endif  /* GCC 2.  */
+# endif	/* limits.h  */
+#endif	/* GCC 2.  */
 
-#endif  /* !_LIBC_LIMITS_H_ */
+#endif	/* !_LIBC_LIMITS_H_ */
 
  /* Get the compiler's limits.h, which defines almost all the ISO constants.
 
@@ -129,26 +130,25 @@ evan@evankali:~$ cat /usr/include/limits.h
    ages are available.  */
 #if defined __USE_ISOC99 && defined __GNUC__
 # ifndef LLONG_MIN
-#  define LLONG_MIN (-LLONG_MAX-1)
+#  define LLONG_MIN	(-LLONG_MAX-1)
 # endif
 # ifndef LLONG_MAX
-#  define LLONG_MAX __LONG_LONG_MAX__
+#  define LLONG_MAX	__LONG_LONG_MAX__
 # endif
 # ifndef ULLONG_MAX
-#  define ULLONG_MAX    (LLONG_MAX * 2ULL + 1)
+#  define ULLONG_MAX	(LLONG_MAX * 2ULL + 1)
 # endif
 #endif
 
-#ifdef  __USE_POSIX
+#ifdef	__USE_POSIX
 /* POSIX adds things to <limits.h>.  */
 # include <bits/posix1_lim.h>
 #endif
 
-#ifdef  __USE_POSIX2
+#ifdef	__USE_POSIX2
 # include <bits/posix2_lim.h>
 #endif
 
-#ifdef  __USE_XOPEN
+#ifdef	__USE_XOPEN
 # include <bits/xopen_lim.h>
 #endif
-
