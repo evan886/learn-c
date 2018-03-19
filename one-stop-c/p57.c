f4a9a1f6646dd36c7c2aa5d4e529a84a60ca268a
evan <evan886@gmail.com>
2018-03-19 19:53:52 +0800

    2018年 03月 19日 星期一 19:53:52 CST

--- one-stop-c/p57.c
+++ one-stop-c/p57.c
@@ -1,7 +1,19 @@
 #include <math.h>
 #include <stdio.h>
-double distance()
+double distance(double x1, double y1, double x2, double y2)
 {
+       double dx = x2 - x1;
+        double dy = y2 - y1;
+        printf("dx is %f\n dy is %f\n",dx,dy);
+        //printf("distance is %f\n",distance(1.0, 2.0, 4.0,6.0));
+        return 0;
+
         return 0.0;
 }
-:
+
+int  main(void)
+{
+       printf("distance is %f\n",distance(1.0, 2.0, 4.0,6.0));
+        return 0;
+}
+
