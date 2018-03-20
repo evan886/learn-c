2f59d241b163394c6ff5cd99cd4a28547be6fe59
evan <evan886@gmail.com>
2018-03-20 15:01:56 +0800

    2018年 03月 20日 星期二 15:01:56 CST

--- one-stop-c/p60.c
+++ one-stop-c/p60.c
@@ -17,31 +17,20 @@ double area(double radius)
         return 3.1416 * radius * radius;
 }
 
-
-        
-        int  main(void)
+double area_point(double x1,  double y1, double x2, double y2)
+{
+        return area(distance(x1, y1, x2, y2));
+}
+int  main(void)
 {
 //       printf("distance is %f\n",distance(1.0, 2.0, 4.0,6.0));
 // distance is 5.000000
-//       printf("area is %f\n",);
-
-       double radius=distance(1.0, 2.0, 4.0,6.0);
-       double result = area(radius);
-printf(" area is %f\n",result);
-// area is 78.540000
-
+// radius=distance(1.0, 2.0, 4.0,6.0);
+        //     double result = area(radius);
+       printf(" area is %f\n",area_point(1.0, 2.x, 4.0, 6.0));
+       // area is 78.540000
        return 0;
 }
-
-/****
-evan@kalipc:~/github/learn-c/one-stop-c$ gcc -Wall  p57.c 
-/tmp/ccbMqPPq.o：在函数‘distance’中：
-p57.c:(.text+0x69)：对‘sqrt’未定义的引用
-collect2: error: ld returned 1 exit status
-
-gcc -Wall -lm   p57.c 
-
-***/
 
 
 
