ce1daf8e9f3066b0846888e50d6c23f14de3fd24
evan <evan886@gmail.com>
2018-03-21 12:26:09 +0800

    2018年 03月 21日 星期三 12:26:09 CST

--- one-stop-c/factorial.c
+++ one-stop-c/factorial.c
@@ -1,5 +1,31 @@
 #include <stdio.h>
 #include <math.h>
+/*
+gcc  -Wall  factorial.c 
+factorial.c: In function ‘main’:
+factorial.c:5:22: warning: implicit declaration of function ‘factorial’ [-Wimplicit-function-declaration]
+         int result = factorial(3);
+                      ^~~~~~~~~
 int main(void)
 {
-        int result = factorial
+        int result = factorial(3);
+        printf("%d\n",result);
+        } */
+
+int factorial(int n)
+{
+        if (n == 0)
+                return 1;
+        else {
+                 int recurse = factorial(n - 1);
+                int result = n * recurse;
+                return result;
+        }
+
+}
+
+int main(void)
+{
+        int result = factorial(3);
+        printf("%d\n",result);
+}
